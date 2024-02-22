f0d5ebed4924fd38e681b91ecab332c15ef273a5
Tom Marshall <tom.q.marshall@gmail.com>
2024-02-21 20:39:28 -0500

    fix indentation

--- src/fmsave.py
+++ src/fmsave.py
@@ -31,66 +31,66 @@ import logging.config
 import yaml
 
 with open('logging.yaml','rt') as f:
-        config=yaml.safe_load(f.read())
-        f.close()
+    config=yaml.safe_load(f.read())
+    f.close()
 logging.config.dictConfig(config)
 
 APP_NAME = 'fmsave'
 logger = logging.getLogger(APP_NAME)
 
 CHROME_OPTIONS = [
-        '--headless',
-        'start-maximized',
-        '--disable-blink-features',
-        '--disable-blink-features=AutomationControlled',
-        ]
+    '--headless',
+    'start-maximized',
+    '--disable-blink-features',
+    '--disable-blink-features=AutomationControlled',
+    ]
 
 if __name__ == '__main__':
-        args = docopt(__doc__)
-        
-        print(args)
-        
-        fm_un = args['FMUSERNAME']
-        gn_un = args['GNUSERNAME']
-        chrome_path = args['CHROME_PATH']
-        save_path = args['SAVE_PATH']
-        
-        dlhtml = args['dlhtml']
-        topd = args['topd']
-        uptz = args['uptz']
-        # if topd:
-        #         SAVE_CSV_FN = args['FN']
-        
-        max_pages = args['--max-html-pages']
-        if max_pages is not None:
-                max_pages = int(max_pages)
-        
-        csv_fn = args['--csvfn']
-        if csv_fn is None:
-                csv_fn = 'flights.csv'
-        
-        fd = FMDownloader(chrome_path=chrome_path, chrome_args=CHROME_OPTIONS)
+    args = docopt(__doc__)
+    
+    print(args)
+    
+    fm_un = args['FMUSERNAME']
+    gn_un = args['GNUSERNAME']
+    chrome_path = args['CHROME_PATH']
+    save_path = args['SAVE_PATH']
+    
+    dlhtml = args['dlhtml']
+    topd = args['topd']
+    uptz = args['uptz']
+    # if topd:
+    #     SAVE_CSV_FN = args['FN']
+    
+    max_pages = args['--max-html-pages']
+    if max_pages is not None:
+        max_pages = int(max_pages)
+    
+    csv_fn = args['--csvfn']
+    if csv_fn is None:
+        csv_fn = 'flights.csv'
+    
+    fd = FMDownloader(chrome_path=chrome_path, chrome_args=CHROME_OPTIONS)
 
-        if dlhtml:
-                # Download and save pages
-                fm_pw = getpass.getpass(prompt="Flight Memory password:")
-                fd.login(username=fm_un, password=fm_pw)
-                fd.get_fm_pages(max_pages=max_pages)
-                fd.save_fm_pages(save_path=save_path)
+    if dlhtml:
+        # Download and save pages
+        fm_pw = getpass.getpass(prompt="Flight Memory password:")
+        fd.login(username=fm_un, password=fm_pw)
+        fd.get_fm_pages(max_pages=max_pages)
+        fd.save_fm_pages(save_path=save_path)
 
-        if topd:
-                # Read in already saved pages
-                fd.read_fm_pages(save_path=save_path)
-                fd.fm_pages_to_pandas()
-                fd.add_lat_lon()
-                fd.add_timezones(gnusername=gn_un)
-                fd.save_pandas_to_csv(save_path=save_path, save_fn=csv_fn)
+    if topd:
+        # Read in already saved pages
+        fd.read_fm_pages(save_path=save_path)
+        fd.fm_pages_to_pandas()
+        fd.add_lat_lon()
+        fd.add_timezones(gnusername=gn_un)
+        fd.save_pandas_to_csv(save_path=save_path, save_fn=csv_fn)
 
-        if uptz:
-                fd.read_pandas_from_csv(save_path=save_path, save_fn=csv_fn)
-                fd.add_timezones(gnusername=gn_un)
-                fd.save_pandas_to_csv(save_path=save_path, save_fn=csv_fn)
+    if uptz:
+        fd.read_pandas_from_csv(save_path=save_path, save_fn=csv_fn)
+        fd.add_timezones(gnusername=gn_un)
+        fd.save_pandas_to_csv(save_path=save_path, save_fn=csv_fn)
 
-        # fd.read_pandas_from_csv(save_path=SAVE_PATH, save_fn=SAVE_CSV_FN)
-        # fd.add_timezones(gnusername=GNUSERNAME, num_flights=10)
-        # fd.save_pandas_to_csv(save_path=SAVE_PATH, save_fn=f"{SAVE_CSV_FN}")
+    # fd.read_pandas_from_csv(save_path=SAVE_PATH, save_fn=SAVE_CSV_FN)
+    # fd.add_timezones(gnusername=GNUSERNAME, num_flights=10)
+    # fd.save_pandas_to_csv(save_path=SAVE_PATH, save_fn=f"{SAVE_CSV_FN}")

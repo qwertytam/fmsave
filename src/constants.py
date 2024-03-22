a62f5f4154eb0a4fb83a6f2188442069f6acdb16
Tom Marshall <tom.q.marshall@gmail.com>
2024-03-21 20:18:35 -0400

    initial commit

---
+++ src/constants.py
@@ -0,0 +1,23 @@
+from datetime import datetime as dt
+from datetime import timedelta as td
+
+DT_INFO_YMDT = "YMDT"
+DT_INFO_YMD = "YMD"
+DT_INFO_YM = "YM"
+DT_INFO_Y = "Y"
+
+STR_TYPE_LU = {
+    'str': str,
+    'float': float,
+    'dt': dt,
+    'td': td,
+}
+
+DEFAULT_CHROME_PATH = '/Applications/Chromium.app/Contents/MacOS/Chromium'
+
+CHROME_OPTIONS = [
+    '--headless',
+    'start-maximized',
+    '--disable-blink-features',
+    '--disable-blink-features=AutomationControlled',
+    ]
